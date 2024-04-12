7e6aea18941d9c68fb0a31e991fe6f3a39e9506b
mltony <anton.malykh+0@gmail.com>
2024-04-12 13:13:31 -0700

    Compatibility with v2024

--- buildVars.py
+++ buildVars.py
@@ -37,9 +37,9 @@ It can also find paragraphs with the same font size or same font style."""),
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": "2019.3.0",
+	"addon_minimumNVDAVersion": "2024.1.0",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2023.1.0",
+	"addon_lastTestedNVDAVersion": "2024.1.0",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
